a6e9ddc5c63b61b280de69ce6acd303e4d842545
pberger <pberger@phys.ethz.ch>
2016-03-30 17:41:53 +0200

    add user test trim2

---
+++ usertests/PixTestTrim2.hh
@@ -0,0 +1,42 @@
+#ifndef PixTestTrim22_H
+#define PixTestTrim22_H
+
+#include "PixTest.hh"
+
+using std::vector;
+
+class DLLEXPORT PixTestTrim2: public PixTest {
+public:
+  PixTestTrim2(PixSetup *, std::string);
+  PixTestTrim2();
+  virtual ~PixTestTrim2();
+  virtual bool setParameter(std::string parName, std::string sval); 
+  void init(); 
+  void setToolTips();
+  void bookHist(std::string); 
+
+  void runCommand(std::string); 
+  void trimTest();
+
+  void setTrimBits(int itrim = -1); 
+  void getCaldelVthrcomp();
+  void doTest();
+  int getNumberOfDeadPixels();
+  void setVthrcomp(int vthrcomp = 50);
+  void getVtrimVthrcomp();
+  double getEfficiency();
+  void retrimStep();
+  void findVthrcomp();
+  
+private:
+
+  int     fParVcal, fParNtrig; 
+  std::vector<std::pair<int, int> > fPIX; 
+  int fTrimBits[16][52][80]; 
+  vector< int > fCaldelVthrcompLUT;
+  int fParNSteps;
+
+  ClassDef(PixTestTrim2, 1)
+
+};
+#endif
